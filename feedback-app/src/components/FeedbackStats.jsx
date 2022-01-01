27af57dc5f5f30238708d3746eb7550166bd7500
eom1228 <chriseom11@gmail.com>
2022-01-02 01:52:06 +0900

    added context, edit and update features

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,7 +1,10 @@
-import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
   // calculate ratings average
+  const { feedback } = useContext(FeedbackContext);
+
   let average =
     feedback.reduce((acc, cur) => {
       return acc + cur.rating;
@@ -17,7 +20,7 @@ function FeedbackStats({ feedback }) {
   );
 }
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
+// FeedbackStats.propTypes = {
+//   feedback: PropTypes.array.isRequired,
+// };
 export default FeedbackStats;
